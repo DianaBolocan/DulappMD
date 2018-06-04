0ea5a075d748f9cecbb44e217eca36268cafb401
Miruna <mirunavasilescu97@gmail.com>
2018-06-04 16:10:45 +0300

    create directory after register

--- App/Controllers/GetStarted.php
+++ App/Controllers/GetStarted.php
@@ -14,13 +14,16 @@
                 		$loginUser ->setUsername($_POST['username']);
                 		$loginUser ->setPassword($_POST['password']);
                 		$loginUser ->setAdmin(0);
-                		$userMapper = new UserMapper();
+                		$userMapper = $this->model('UserMapper');
                 		if(!($userMapper ->usernameExists($loginUser))){
                 			if(!($userMapper ->save($loginUser))){
                 				echo "Probleme la insert";
                 			}
                 			else{
                 				//$this->view('Login');
+                                 if (!file_exists('CSS Files/Uploads/New')) {
+                                    mkdir('CSS Files/Uploads/New', 0777, true);
+                                }
                                  header('Location: '.' http://localhost/DulappMD/Public/Login');
                                  //die();
                              }
