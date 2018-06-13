fd4b252d7337a23ec6b31a34f9b135b829acaedc
Diana Bolocan <DianaBolocan@users.noreply.github.com>
2018-06-13 19:08:14 +0300

    RSS feed
    Creates a RSS feed

--- App/Controllers/DulapSelected.php
+++ App/Controllers/DulapSelected.php
@@ -78,6 +78,10 @@
 		}
 		public function main(){
 			$wardrobeID=$_SESSION['wardrobeID'];
+			$actionMapper = $this->mapper('ActionMapper');
+			$selectResult= $actionMapper->selectActions($wardrobeID);
+			$posts = array();
+
 			if($_POST){
     			if(isset($_POST['Update'])){
     				$newName=($_POST['newName']);
@@ -87,9 +91,6 @@
     				header('Location: '.' http://localhost/DulappMD/Public/DulapSelected?wardrobeID='. $wardrobeID);
     			}
     			if(isset($_POST['exportJSON'])){
-					$actionMapper = $this->mapper('ActionMapper');
-					$selectResult= $actionMapper->selectActions($wardrobeID);
-					$posts = array();
 					for($i=0;$i<sizeof($selectResult);$i++){
 						//$currentItem looks like : "wardrobeID_action_momentofAction"
 						$currentItem=$selectResult[$i];
@@ -119,7 +120,35 @@
 					readfile($filename);
 
 					exit;
+				}
+				if(isset($_POST['exportRSS'])){
+					$results = '<rss version="2.0"><channel><title>Wardrobe ' . $wardrobeID . ' News</title><link>http://localhost/DulappMD/Public</link><description>Changes in wardrobe.</description><language>en-us</language>';
+					for($i=0;$i<sizeof($selectResult);$i++){
+						//$currentItem looks like : "wardrobeID_action_momentofAction"
+						$currentItem=$selectResult[$i];
+						$pieces = explode("_", $currentItem);
+						//echo $pieces[0]; // piece1
+						//echo $pieces[1]; // piece2
+						$results = $results . '<item><title>' . $pieces[1] . '</title><description>' . $pieces[3] . '</description><pubDate>' . date('r',strtotime($pieces[2])) . '</pubDate></item>';
+					}
+					$results = $results . '</channel></rss>';
+					$filename = 'wardrobeID' . $pieces[0] . '.xml';
+					$fp = fopen($filename,'w');
+					fwrite($fp, $results);
+					fclose($fp);
 
+					header('Pragma: public');
+					header('Expires: 0');
+					header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+					header('Cache-Control: private', false); // required for certain browsers 
+					header('Content-Type: text/xml');
+					header('Content-Transfer-Encoding: binary');
+					header('Content-Disposition: attachment; filename="' . basename($filename) . '"');
+					header('Content-Length: ' . filesize($filename));
+
+					readfile($filename);
+
+					exit;
 				}
 		}
 	}
